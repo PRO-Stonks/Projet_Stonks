b85cf36e6581d96a5d00c391f4919879eb7e0220
K-do <miguel.dovalelopes@heig-vd.ch>
2021-04-02 15:37:30 +0200

    fixed address to have a type: {street, noStreet, ...}

--- models/locationModel.js
+++ models/locationModel.js
@@ -7,13 +7,15 @@ const locationSchema = new mongoose.Schema({
         required: [true, "Please provide a name"],
     },
     address: {
-        street: {type: String, required: [true, "Please provide a street name"]},
-        noStreet: {type: Number, required: [true, "Please provide a street number"]},
-        npa: {type: Number, required: [true, "Please provide a NPA"]},
-        city: {type: String, required: [true, "Please provide a city name"]},
-        country: {type: String, required: [true, "Please provide the country name"]},
+        type: {
+            street: {type: String, required: [true, "Please provide a street name"]},
+            noStreet: {type: Number, required: [true, "Please provide a street number"]},
+            npa: {type: Number, required: [true, "Please provide a NPA"]},
+            city: {type: String, required: [true, "Please provide a city name"]},
+            country: {type: String, required: [true, "Please provide the country name"]},
+        },
         required: [true, "Please provide an address"]
-    },
+    }
 });
 
 const Location = mongoose.model("Location", locationSchema);
