31525184af84fd08a9586c78402e7f83c161fb2e
K-do <miguel.dovalelopes@heig-vd.ch>
2021-04-02 14:35:51 +0200

    added Location model and fixed product model

--- models/productModel.js
+++ models/productModel.js
@@ -15,7 +15,7 @@ const productSchema = new mongoose.Schema({
         default: true,
         select: false,
     },
-})
+});
 
 const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
