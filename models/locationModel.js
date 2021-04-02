b78962d9cf9c7c595065527925bbf3b23778664e
K-do <miguel.dovalelopes@heig-vd.ch>
2021-04-02 15:34:30 +0200

    added required property to object adress

--- models/locationModel.js
+++ models/locationModel.js
@@ -11,7 +11,8 @@ const locationSchema = new mongoose.Schema({
         noStreet: {type: Number, required: [true, "Please provide a street number"]},
         npa: {type: Number, required: [true, "Please provide a NPA"]},
         city: {type: String, required: [true, "Please provide a city name"]},
-        country: {type: String, required: [true, "Please provide the country name"]}
+        country: {type: String, required: [true, "Please provide the country name"]},
+        required: [true, "Please provide an address"]
     },
 });
 
