e67b94282296d87aa681e3d616561c82fe256957
K-do <miguel.dovalelopes@heig-vd.ch>
2021-04-06 00:38:57 +0200

    added Product route

---
+++ routes/productRoutes.js
@@ -0,0 +1,23 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const productController = require('../controllers/productController');
+const authController = require('./../controllers/authController');
+
+// Protect all routes after this middleware
+router.use(authController.protect);
+
+router.post('/getOne', productController.getProduct);
+router.post('/getAll', productController.getAllProduct);
+
+// Only admin have permission to access for the below APIs
+router.use(authController.restrictTo('admin'));
+
+router.delete('/delete', productController.deleteProduct);
+router.delete('/softDelete', productController.softDeleteProduct);
+
+router
+    .route('/:id')
+    .patch(productController.updateProduct);
+
+module.exports = router;
