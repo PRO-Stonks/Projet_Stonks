3170958efc2dae64eacf31b86985a8e0502ae0c3
LeonardBesseau <leonard.besseau@heig-vd.ch>
2021-04-05 12:53:33 +0200

    Added EventModel structure

--- models/eventModel.js
+++ models/eventModel.js
@@ -3,9 +3,14 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
+const eventSchema = new mongoose.Schema({
+    time: {
+        type: Date,
+        default: Date.now
+    }
+}, {discriminatorKey: 'kind'});
 
-});
 
-const Event = mongoose.model("Event", userSchema);
+
+const Event = mongoose.model("Event", eventSchema);
 module.exports = Event;
