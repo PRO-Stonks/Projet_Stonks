aa84e72c50414bed722fec5b37a7baf0fc87a976
K-do <miguel.dovalelopes@heig-vd.ch>
2021-04-05 23:59:12 +0200

    added productController

---
+++ controllers/productController.js
@@ -0,0 +1,27 @@
+'use strict';
+const Product = require("../models/productModel");
+const base = require("./baseController");
+
+exports.softDeleteProduct = async (req, res, next) => {
+    // As i understood, we won't remove the products to keep track on logs
+    // we'll just put a variable to false
+    // maybe we should add a methide named "softDelete" on baseController ?
+    try {
+        Product.findByIdAndUpdate(req.product.id, {
+            active: false
+        });
+
+        res.status(204).json({
+            status: 'success',
+            data: null
+        })
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.getProduct = base.getOne(Product);
+exports.getAllProduct = base.getAll(Product);
+exports.deleteProduct = base.deleteOne(Product);
+exports.updateProduct = base.updateOne(Product);
