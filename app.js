2cdb3e9b4fd8cdffb67fb38a226312755b9724e6
K-do <miguel.dovalelopes@heig-vd.ch>
2021-04-06 00:59:36 +0200

    add route/products to app

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const cors = require('cors');
 
 
 const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
 const globalErrHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
 const app = express();
@@ -44,6 +45,7 @@ app.use(hpp());
 
 // Routes
 app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/products', productRoutes);
 
 // handle undefined Routes
 app.use('*', (req, res, next) => {
