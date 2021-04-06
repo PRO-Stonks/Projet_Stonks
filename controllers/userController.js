375b446e658cc46ba665c433af6bbbe07e85a7ba
LeonardBesseau <leonard.besseau@heig-vd.ch>
2021-04-06 15:30:13 +0200

    Corrected userController. User should not be able to delete themself. Only admin

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,23 +2,6 @@
 const User = require('../models/userModel');
 const base = require('./baseController');
 
-exports.deleteMe = async (req, res, next) => {
-    try {
-        await User.findByIdAndUpdate(req.user.id, {
-            active: false
-        });
-
-        res.status(204).json({
-            status: 'success',
-            data: null
-        });
-
-
-    } catch (error) {
-        next(error);
-    }
-};
-
 exports.getAllUsers = base.getAll(User);
 exports.getUser = base.getOne(User);
 
