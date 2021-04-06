4707a2c68e5b2d5c26c845e831ed3fd8f80ffd5a
LeonardBesseau <leonard.besseau@heig-vd.ch>
2021-04-06 15:31:00 +0200

    Corrected userController. User should not be able to delete themself. Only admin

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,7 +11,6 @@ router.post('/signup', authController.signup);
 // Protect all routes after this middleware
 router.use(authController.protect);
 
-router.delete('/deleteMe', userController.deleteMe);
 
 // Only admin have permission to access for the below APIs
 router.use(authController.restrictTo('admin'));
