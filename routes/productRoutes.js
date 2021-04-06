5aad6faa3c3d0880c5463dc5bd16a87395c46f31
K-do <miguel.dovalelopes@heig-vd.ch>
2021-04-06 16:11:40 +0200

    finished productRoutes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -7,18 +7,27 @@ const authController = require('./../controllers/authController');
 // Protect all routes after this middleware
 router.use(authController.protect);
 
-router.post('/getOne', productController.getProduct);
-router.post('/getAll', productController.getAllProduct);
+// Available by every user
+router
+    .route('/:id')
+    .get(productController.getProduct);
+
+router
+    .route('/')
+    .get(productController.getAllProduct);
 
 // Only admin have permission to access for the below APIs
 router.use(authController.restrictTo('admin'));
 
-router.delete('/delete', productController.deleteProduct);
-router.delete('/softDelete', productController.softDeleteProduct);
 router.post('/add', productController.addProduct);
 
 router
     .route('/:id')
-    .patch(productController.updateProduct);
+    .patch(productController.updateProduct)
+    .delete(productController.softDeleteProduct);
+
+router
+    .route('/hardDel/:id')
+    .delete(productController.deleteProduct);
 
 module.exports = router;
