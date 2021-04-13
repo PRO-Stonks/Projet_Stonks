622178aefb0e6ccbe52a9b16350e055838974730
K-do <miguel.dovalelopes@heig-vd.ch>
2021-04-13 22:56:19 +0200

    removed and reforged App without css for now

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,12 @@
-import logo from './assets/stonks4.png';
-import './App.css';
 import LogInForm2 from "./login/LogInForm2";
-import MainPage from "./MainPage";
 import React, {useEffect, useState} from "react";
+
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Navigation from "./pages/Navigation";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Footer from "./pages/Footer";
+import logo from "./assets/stonks4.png";
 
 function App() {
     const [state, setState] = useState({loggedIn: false, user: {}, token: ""});
@@ -14,6 +18,12 @@ function App() {
         localStorage.setItem("user", JSON.stringify(e.user));
     }
 
+    const handleLogOut = () => {
+        setState({loggedIn: false, user: {}, token: ""});
+        localStorage.setItem("token", "");
+        localStorage.setItem("user", "");
+    }
+
 
     useEffect(() => {
         let token = localStorage.getItem("token");
@@ -21,10 +31,10 @@ function App() {
             token = token.split('.');
             const payload = JSON.parse(atob(token[1]));
             const current_time = Date.now().valueOf() / 1000;
-            if(payload.exp < current_time){
+            if (payload.exp < current_time) {
                 localStorage.removeItem("token");
                 localStorage.removeItem("user");
-            }else{
+            } else {
                 setState(
                     {
                         token,
@@ -35,18 +45,50 @@ function App() {
             }
         }
     }, []);
+
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p className="Logo-text-down">Not Stonks</p>
-                <p className="Logo-text-up">Stonks</p>
-                {state.loggedIn ? <MainPage user={state.user} token={state.token}/> :
-                    <LogInForm2 handleChangeProps={handleChange}/>}
-            </header>
+            <Router>
+                <Navigation handleLogOut={handleLogOut}/>
+                <Switch>
+                    <Route path="/" exact component={() => state.loggedIn ?
+                        <HomePage user={state.user} token={state.token}/> :
+                        <div>
+                            <div className="welcome">
+                                <div className="container">
+                                    <div className="row align-items-center my-5">
+                                        <div className="col-lg-7">
+                                            <img src={logo} className="App-logo" alt="logo"/>
+                                            <h1 className="font-weight-light">Stonks</h1>
+                                            <p>
+                                                Welcome to Stonks !
+                                            </p>
+                                        </div>
+                                        <div className="col-lg-5">
+                                            <h1 className="font-weight-light">Log-in</h1>
+                                            <LogInForm2 handleChangeProps={handleChange}/>
+                                            <br/>
+                                            <h1 className="font-weight-light">Sign-up</h1>
+                                            #TODO
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    }/>
+                    <Route path="/about" exact component={() => <About/>}/>
+                </Switch>
+                <Footer/>
+            </Router>
         </div>
     );
 
+    /* image de l'apocalypse
+    <img src={logo} className="App-logo" alt="logo"/>
+                <p className="Logo-text-down">Not Stonks</p>
+                <p className="Logo-text-up">Stonks</p>
+     */
 }
 
 export default App;
