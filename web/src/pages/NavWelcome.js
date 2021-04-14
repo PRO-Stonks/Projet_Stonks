7441cd9ca9f38ebe2586db0ca55f3f064940c6cb
K-do <miguel.dovalelopes@heig-vd.ch>
2021-04-14 22:15:26 +0200

    splited nav bar into 2 entities (admin and welcome)

--- web/src/pages/NavWelcome.js
+++ web/src/pages/NavWelcome.js
@@ -4,40 +4,25 @@ import logo from "../assets/Webp.net-resizeimage.png"
 
 function NavWelcome(props) {
     return (
-        <div className="navigation">
-            <nav class="navbar navbar-expand navbar-dark bg-dark">
-                <div class="container">
-                    <Link class="navbar-brand" to="/">
+        <div className="navigationWelcome">
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+                <div className="container">
+                    <Link className="navbar-brand" to="/">
                         <img src={logo} alt="logo"/>   Stonks
                     </Link>
                     <div>
-                        <ul class="navbar-nav ml-auto">
-                            <li
-                                class={`nav-item  ${
-                                    props.location.pathname === "/" ? "active" : ""
-                                }`}
-                            >
+                        <ul className="navbar-nav ml-auto">
+                            <li className={`nav-item ${props.location.pathname === "/" ? "active" : ""}`}>
                                 <Link class="nav-link" to="/">
                                     Home
                                     <span class="sr-only">(current)</span>
                                 </Link>
                             </li>
-                            <li
-                                class={`nav-item  ${
-                                    props.location.pathname === "/about" ? "active" : ""
-                                }`}
-                            >
+                            <li className={`nav-item ${props.location.pathname === "/about" ? "active" : ""}`}>
                                 <Link class="nav-link" to="/about">
                                     About
                                 </Link>
                             </li>
-                            { localStorage.token ?
-                                <li className={`nav-item`}>
-                                    <Link class="nav-link">
-                                        <button onClick={props.handleLogOut}>Log out</button>
-                                    </Link>
-                                </li> : ""
-                            }
                         </ul>
                     </div>
                 </div>
