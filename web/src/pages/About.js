4fde18883dfd1199321d5b83daadaa0c0e9e279c
K-do <miguel.dovalelopes@heig-vd.ch>
2021-04-14 22:12:21 +0200

    changed about and footer pages

--- web/src/pages/About.js
+++ web/src/pages/About.js
@@ -1,24 +1,28 @@
 import React from "react";
+import logo from "../assets/stonks4.png";
 
 function About() {
     return (
         <div className="about">
-            <div class="container">
-                <div class="row align-items-center my-5">
-                    <div class="col-lg-7">
-                        <img
-                            class="img-fluid rounded mb-4 mb-lg-0"
-                            src="http://placehold.it/900x400"
-                            alt=""
-                        />
+            <div className="container">
+                <div className="row align-items-center my-5">
+                    {/* #TODO remove later */}
+                    <div className="col-lg-7">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <p className="Logo-text-down">Not Stonks</p>
+                        <p className="Logo-text-up">Stonks</p>
                     </div>
-                    <div class="col-lg-5">
-                        <h1 class="font-weight-light">About</h1>
-                        <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting
-                            industry. Lorem Ipsum has been the industry's standard dummy text
-                            ever since the 1500s, when an unknown printer took a galley of
-                            type and scrambled it to make a type specimen book.
+                    
+                    <div className="col-lg-5">
+                        <h1 className="font-weight-light">About</h1>
+                        <h4>Stonks project</h4>
+                        <p> For the HEIG-VD PRO course. </p>
+                        <p> Authors:<br/>
+                            Besseau Léonard<br/>
+                            Cerottini Alexandra<br/>
+                            Do Vale Lopes Miguel<br/>
+                            Gamboni Fiona<br/>
+                            Tevaearai Rebecca
                         </p>
                     </div>
                 </div>
--- web/src/pages/Footer.js
+++ web/src/pages/Footer.js
@@ -3,10 +3,10 @@ import React from "react";
 function Footer() {
     return (
         <div className="footer">
-            <footer class="py-5 bg-dark fixed-bottom">
-                <div class="container">
-                    <p class="m-0 text-center text-white">
-                        Copyright &copy; Your Website 2020
+            <footer className="py-5 bg-dark fixed-bottom">
+                <div className="container">
+                    <p className="m-0 text-center text-white">
+                        Copyright &copy; Stonks 2021
                     </p>
                 </div>
             </footer>
