cbd055d99093cb108ee54d7a758f5ae1183079f4
Insuline13 <alexandra.cerottini@heig-vd.ch>
2021-04-15 11:17:58 +0200

    new filename + new test

--- STONKS/screens/Ajouter.js
+++ STONKS/screens/Ajouter.js
@@ -1,7 +1,42 @@
-import React, { useState } from "react";
+import React from "react";
+import RNPickerSelect from "react-native-picker-select";
+import { StyleSheet, Text, View, TextInput } from "react-native";
+
+
+export default function Ajouter () {
+    return (
+        <View style={styles.container}>
+            <Text>Hello World!</Text>
+
+            <RNPickerSelect
+                onValueChange={(value) => console.log(value)}
+                items={[
+                    { label: "JavaScript", value: "JavaScript" },
+                    { label: "TypeStript", value: "TypeStript" },
+                    { label: "Python", value: "Python" },
+                    { label: "Java", value: "Java" },
+                    { label: "C++", value: "C++" },
+                    { label: "C", value: "C" },
+                ]}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container : {
+        flex            : 1,
+        backgroundColor : "#fff",
+        alignItems      : "center",
+        justifyContent  : "center",
+    },
+});
+
+
+/*import React, { useState } from "react";
 import {FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity} from "react-native";
 
-//export default function Visualiser() {
+//export default function Ajouter() {
 
 
     const DATA = [
@@ -51,7 +86,7 @@ import {FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TextInput, Touchabl
 
 
 
-const Visualiser = () => {
+const Ajouter = () => {
         const [selectedId, setSelectedId] = useState(null);
 
         const renderItem = ({item}) => {
@@ -100,5 +135,6 @@ const styles = StyleSheet.create({
         fontSize: 22,
     },
 });
-export default Visualiser;
+export default Ajouter;
 
+*/
