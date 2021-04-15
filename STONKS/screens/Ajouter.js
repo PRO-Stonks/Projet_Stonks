93c9fa555b7616aa8afc3b656bc660c6f01ef902
Insuline13 <alexandra.cerottini@heig-vd.ch>
2021-04-15 11:27:42 +0200

    success to add inputText for price

--- STONKS/screens/Ajouter.js
+++ STONKS/screens/Ajouter.js
@@ -1,140 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import RNPickerSelect from "react-native-picker-select";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import {StyleSheet, Text, View, TextInput} from "react-native";
 
 
-export default function Ajouter () {
+export default function Ajouter() {
+    const [text, setText] = useState('');
     return (
         <View style={styles.container}>
-            <Text>Hello World!</Text>
+            <Text>Price</Text>
+            <TextInput
+                style={{height: 40}}
+                placeholder="Price of the product in CHF"
+                onChangeText={text => setText(text)}
+                defaultValue={text}
+                keyboardType={"decimal-pad"}
+            />
 
             <RNPickerSelect
                 onValueChange={(value) => console.log(value)}
                 items={[
-                    { label: "JavaScript", value: "JavaScript" },
-                    { label: "TypeStript", value: "TypeStript" },
-                    { label: "Python", value: "Python" },
-                    { label: "Java", value: "Java" },
-                    { label: "C++", value: "C++" },
-                    { label: "C", value: "C" },
+                    {label: "JavaScript", value: "JavaScript"},
+                    {label: "TypeStript", value: "TypeStript"},
+                    {label: "Python", value: "Python"},
+                    {label: "Java", value: "Java"},
+                    {label: "C++", value: "C++"},
+                    {label: "C", value: "C"},
                 ]}
             />
+
+
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container : {
-        flex            : 1,
-        backgroundColor : "#fff",
-        alignItems      : "center",
-        justifyContent  : "center",
-    },
-});
-
-
-/*import React, { useState } from "react";
-import {FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity} from "react-native";
-
-//export default function Ajouter() {
-
-
-    const DATA = [
-        {
-            id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-            title: "First Item",
-        },
-        {
-            id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-            title: "Second Item",
-        },
-        {
-            id: "58694a0f-3da1-471f-bd96-145571e29d72",
-            title: "Third Item",
-        },
-        {
-            id: "58694a0f-3da1-471f-bd96-145571e29d72",
-            title: "Third Item",
-        },
-        {
-            id: "58694a0f-3da1-471f-bd96-145571e29d72",
-            title: "Third Item",
-        },
-        {
-            id: "58694a0f-3da1-471f-bd96-145571e29d72",
-            title: "Third Item",
-        },
-        {
-            id: "58694a0f-3da1-471f-bd96-145571e29d72",
-            title: "Third Item",
-        },
-        {
-            id: "58694a0f-3da1-471f-bd96-145571e29d72",
-            title: "Third Item",
-        },
-        {
-            id: "58694a0f-3da1-471f-bd96-145571e29d72",
-            title: "Third Item",
-        },
-    ];
-
-    const Item = ({item, onPress, backgroundColor, textColor}) => (
-        <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
-            <Text style={[styles.title, textColor]}>{item.title}</Text>
-        </TouchableOpacity>
-    );
-
-
-
-const Ajouter = () => {
-        const [selectedId, setSelectedId] = useState(null);
-
-        const renderItem = ({item}) => {
-            const backgroundColor = item.id === selectedId ? "#C08924" : "#FAA91A";
-            const color = item.id === selectedId ? 'white' : 'black';
-
-            return (
-                <Item
-                    item={item}
-                    onPress={() => setSelectedId(item.id)}
-                    backgroundColor={{backgroundColor}}
-                    textColor={{color}}
-                />
-            );
-        };
-
-
-        return (
-
-            <SafeAreaView style={styles.container}>
-
-                <FlatList
-                    data={DATA}
-                    renderItem={renderItem}
-                    keyExtractor={(item) => item.id}
-                    extraData={selectedId}
-                />
-            </SafeAreaView>
-        );
-    };
-
-//}
-
-
-const styles = StyleSheet.create({
     container: {
         flex: 1,
-        marginTop: StatusBar.currentHeight || 0,
-    },
-    item: {
-        padding: 10,
-        marginVertical: 6,
-        marginHorizontal: 90,
-    },
-    title: {
-        fontSize: 22,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
     },
 });
-export default Ajouter;
-
-*/
