e015a410acf662975f5489b9dfcda3b76e2c0957
FionaGbn <fiona.gamboni@heig-vd.ch>
2021-04-15 22:12:50 +0200

    Cleaned Menu screen

--- STONKS/screens/Menu.js
+++ STONKS/screens/Menu.js
@@ -3,35 +3,7 @@ import React, {useState, useEffect, useContext} from 'react';
 import Options from "./Options";
 import API_URL from "../url";
 import {UserContext} from "../UserContext";
-
-/**
- * Fetch product
- * @param url : path
- * @param token : user token
- * @returns {Promise<any>} : response
- */
-async function getData(url, token){
-    //console.log(token)
-    try{
-        const response = await fetch(url, {
-            method: 'GET', // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors', // no-cors, *cors, same-origin
-            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'same-origin', // include, *same-origin, omit
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + token
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            redirect: 'follow', // manual, *follow, error
-            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        });
-        return response.json();
-    }catch (e){
-        console.log("Error")
-        console.log(e);
-    }
-}
+import getProducts from '../request/getProducts.js';
 
 
 export default function Menu(props) {
@@ -48,19 +20,17 @@ export default function Menu(props) {
      */
     const [isFetchingProduct, setIsFetchingProduct] = useState(false);
 
-
     /**
      * Get products when we get on this screen
      */
     async function fetchData() {
-        const res = await getData(API_URL + 'products', userData.state.token);
+        const res = await getProducts(API_URL + 'products', userData.state.token);
         if (res.status === 'success') {
             return res;
         } else {
             throw res;
         }
     }
-
 
     useEffect(() => {
         fetchData().then(r => setProduct(r)).catch(r => console.log(r))
