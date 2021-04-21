2154fda3a0260e5f6e62078e67343fc4389843c6
LeonardBesseau <leonard.besseau@heig-vd.ch>
2021-04-21 15:58:30 +0200

    Corrected session error due to session beeing not started

--- Server/controllers/elementController.js
+++ Server/controllers/elementController.js
@@ -45,7 +45,9 @@ exports.softDeleteElement = async (req, res, next) => {
     } catch (error) {
         next(error);
     } finally {
-        session.endSession();
+        if(session){
+            session.endSession();
+        }
     }
 };
 
@@ -120,7 +122,9 @@ exports.addElement = async (req, res, next) => {
             next(err);
         }
     } finally {
-        session.endSession();
+        if(session){
+            session.endSession();
+        }
     }
 };
 
@@ -176,7 +180,9 @@ exports.moveElement = async (req, res, next) => {
     } catch (error) {
         next(error);
     } finally {
-        session.endSession();
+        if(session){
+            session.endSession();
+        }
     }
 };
 
