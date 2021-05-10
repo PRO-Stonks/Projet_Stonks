3ece62dc6836b97266814d80cdbfd5775507f868
K-do <miguel.dovalelopes@heig-vd.ch>
2021-05-10 20:54:03 +0200

    Product now handles active objects

--- web/src/admin/product/ProductdetailsForm/ProductListElement.js
+++ web/src/admin/product/ProductdetailsForm/ProductListElement.js
@@ -1,23 +1,27 @@
 import React from "react";
 
 function ProductListElement({item}) {
-    return <div className="container-fluid">
-        <label className="form-check-label">id</label>
-        <div
-            className="form-control w-50">{item._id}</div>
-        <label className="form-check-label">name</label>
-        <div
-            className="form-control w-50">{item.name}</div>
-        <label className="form-check-label">tag</label>
-        <textarea
-            className="form-control w-50 "
-            name="tag"
-            value={item.tag}
-            disabled
-        />
-        <br/>
-        <br/>
-    </div>;
+    if (item.active) {
+        return <div className="container-fluid">
+            <label className="form-check-label">id</label>
+            <div
+                className="form-control w-50">{item._id}</div>
+            <label className="form-check-label">name</label>
+            <div
+                className="form-control w-50">{item.name}</div>
+            <label className="form-check-label">tag</label>
+            <textarea
+                className="form-control w-50 "
+                name="tag"
+                value={item.tag}
+                disabled
+            />
+            <br/>
+            <br/>
+        </div>;
+    } else {
+        return "";
+    }
 }
 
 export default ProductListElement;
